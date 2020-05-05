8cb38c70218fcd80a1a0eee627cf81d0d4581602
Joshua Engel <engel@highfleet.com>
2020-05-05 16:19:05 -0400

    Initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Map, TileLayer, CircleMarker} from 'react-leaflet'
+
+import SiteList from "./components/SiteList";
 import './App.css';
 
+const MAX_ZOOM = 18;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const [viewport, setViewport] = useState();
+    const [position, setPosition] = useState();
+    const [init, setInit] = useState(false);
+
+    if (navigator.geolocation && !init) {
+        setInit(true);
+        navigator.geolocation.watchPosition((position) => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude;
+                setPosition([latitude, longitude]);
+            },
+            function error(msg) {
+                console.error('Geolocation error', msg.message);
+            },
+            {enableHighAccuracy: true});
+    } else {
+        // alert("Geolocation API is not supported in your browser.");
+    }
+    const vp = viewport || (position && {center: position, zoom: MAX_ZOOM})
+
+    const me = position &&
+        <CircleMarker center={{lat: position[0], lng: position[1]}} radius={5}/>
+    return <div className='App'>
+        <Map viewport={vp} onViewportChanged={setViewport}>
+            <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {me}
+            <SiteList/>
+        </Map>
     </div>
-  );
 }
 
 export default App;
---
+++ src/components/SiteList.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function SiteList({}) {
+    return <div>
+        <h1>Places</h1>
+    </div>
+}
