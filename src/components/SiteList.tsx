749695a144e4c0d1ea6449a3cad756bb17f4c4f2
Joshua Engel <engel@highfleet.com>
2020-05-06 18:47:41 -0400

    Styling site list

--- src/components/SiteList.tsx
+++ src/components/SiteList.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import {FeatureCollection, Feature, Geometry, GeoJsonProperties} from 'geojson';
+import {ButtonBase} from "@material-ui/core";
 
 
-export default function SiteList({tour, onClick}:
+export default function SiteList({tour, current, onClick}:
                                      {
                                          tour: FeatureCollection,
                                          current: Feature,
@@ -10,8 +11,15 @@ export default function SiteList({tour, onClick}:
                                      }) {
 
     function displayFeature(feature: Feature<Geometry, GeoJsonProperties>, i: number) {
+        const backgroundColor = feature === current ? 'lightblue' : undefined;
         return feature.properties && feature.properties.name &&
-          <a href='#' onClick={() => onClick(feature)} key={i}>{feature.properties.name}</a>
+            <div key={i}>
+              <ButtonBase
+                style={{textAlign: 'left', textIndent: 0, backgroundColor }}
+                onClick={() => onClick(feature)}>
+                  {feature.properties.name.trim()}
+              </ButtonBase>
+            </div>
     }
 
     return <div>
