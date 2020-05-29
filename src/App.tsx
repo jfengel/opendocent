5eaa220825dff3ae9c27f5d629a4b190d8556cc0
Joshua Engel <engel@highfleet.com>
2020-05-29 13:56:24 -0400

    Clean up initialization

--- src/App.tsx
+++ src/App.tsx
@@ -60,32 +60,32 @@ const onRedirectCallback = () => {
 function App() {
     const [viewport, setViewport] = useState();
     const [position, setPosition] = useState();
-    const [init, setInit] = useState(false);
     const [currentFeature, setCurrentFeature] = useState<Placemark|null>(null);
     const [tour, setTour] = useState<Document>();
     const [availableTours, setAvailableTours] = useState<Document[]>([]);
 
-    if (navigator.geolocation && !init) {
-        setInit(true);
-        let lastUpdate = 0;
-        navigator.geolocation.watchPosition((position) => {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
-                if(!isNaN(longitude) && !isNaN(latitude)) {
-                    const now = new Date().valueOf()
-                    if(now - lastUpdate > GEOLOCATION_UPDATE_FREQUENCY_MSEC) {
-                        setPosition([latitude, longitude]);
-                        lastUpdate = now;
+    useEffect(() => {
+        if (navigator.geolocation) {
+            let lastUpdate = 0;
+            navigator.geolocation.watchPosition((position) => {
+                    const latitude = position.coords.latitude;
+                    const longitude = position.coords.longitude;
+                    if (!isNaN(longitude) && !isNaN(latitude)) {
+                        const now = new Date().valueOf()
+                        if (now - lastUpdate > GEOLOCATION_UPDATE_FREQUENCY_MSEC) {
+                            setPosition([latitude, longitude]);
+                            lastUpdate = now;
+                        }
                     }
-                }
-            },
-            function error(msg) {
-                console.error('Geolocation error', msg.message);
-            },
-            {enableHighAccuracy: true});
-    } else {
-        // alert("Geolocation API is not supported in your browser.");
-    }
+                },
+                function error(msg) {
+                    console.error('Geolocation error', msg.message);
+                },
+                {enableHighAccuracy: true});
+        } else {
+            // alert("Geolocation API is not supported in your browser.");
+        }
+    }, [])
 
     useEffect(() => {
         fetchTourList().then(setAvailableTours);
