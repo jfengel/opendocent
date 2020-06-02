3316bc0dd7af38d0ab87589a7bfaaa022d66f350
Joshua Engel <engel@purgo.net>
2020-06-02 14:36:37 -0400

    Add skeleton while loading tour list

--- src/components/TourList.tsx
+++ src/components/TourList.tsx
@@ -1,11 +1,18 @@
 import {Document} from "../Kml";
 import {Link} from "@material-ui/core";
+import Skeleton from '@material-ui/lab/Skeleton';
 import React from "react";
 
 export function TourList({availableTours, setTour}: {
     availableTours: Document[],
     setTour: (value: Document) => void
 }) {
+    if(availableTours.length === 0) {
+        return <div style={{width: "75%"}}>{
+            Array(10).map((_, i) =>
+                <Skeleton key={i} variant="text"/>)
+        }</div>
+    }
     return <div>
         <h1>Take a tour</h1>
         <div>
